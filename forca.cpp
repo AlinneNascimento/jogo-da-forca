82923566c5d7147e26cce71fd1acace9d76e7fc7
Alinne Nascimento <alinnekellynascimento@gmail.com>
2024-02-28 09:48:17 -0300

    Projeto inicial

---
+++ forca.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute){
+	for (int i = 0; i < PALAVRA_SECRETA.size(); i++){
+		if (chute == PALAVRA_SECRETA[i]){
+			return true;
+		}		
+	}
+	return false;
+}
+
+int main(){	
+	cout << PALAVRA_SECRETA<< endl;
+
+	bool nao_acertou = true;
+	bool nao_enforcou = true;
+
+	while (nao_acertou && nao_enforcou){
+		char chute;
+		cin >> chute;
+
+		if (letra_existe(chute)) {
+			cout << "Você acertou! Seu chute está na palavra.\n";
+		}else {
+			cout << "Você errou! Seu chute não está na palavra.\n";
+
+		}
+
+	}
+}
